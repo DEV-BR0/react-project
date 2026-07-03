5029f9401319df7318d6b4bd888bc448a312f079
ubaydulloh <lazizbekxoljigitov@gmail.com>
2026-07-03 15:18:26 +0500

    sanr

--- src/pages/cotegoriy/Cotegoriy.jsx
+++ src/pages/cotegoriy/Cotegoriy.jsx
@@ -1,12 +1,24 @@
+import { Plus } from "lucide-react";
 
 function Cotegoriy() {
   return (
-    <div> 
-      <div className="flex  justify-center">
-        <div className="flex"></div>
-        </div>    
+    <div className="app flex flex-col gap-[10px]">
+      <div className="flex-col gap-[20px] flex lg:flex-row  justify-between w-full">
+        <div className="left">
+          <p className="text-[27px] font-bold">Tranzaksiyalar</p>
+          <p className="text-gray-500">Barcha xarajat va daromadlar</p>
+        </div>
+        <div className="right">
+          <button
+            onClick={() => setModal("add")}
+            className="flex gap-[10px] p-[10px] bg-black rounded-xl text-white hover:bg-black/70 transition duration-300"
+          >
+            <Plus /> Yangi tranzaksiya
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cotegoriy
+export default Cotegoriy;
